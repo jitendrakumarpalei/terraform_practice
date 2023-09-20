c6d02d5d720708eff5ec71e0fcb57f499e01ca27
jitu <jitendrakumarpalei6@gmail.com>
2023-09-20 10:44:52 +0530

    file

---
+++ .templates/multifile.tf
@@ -0,0 +1,12 @@
+output "firstoutputblock" {
+  value = "this is first hello world block"
+}
+
+
+output "secoundoutputblock" {
+  value = "this is first hello world block"
+}
+
+output "thirdoutputblock" {
+  value = "this is secound hello world block"
+}
